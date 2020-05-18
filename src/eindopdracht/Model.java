2ed6bc5c109eaa14a44365116a5132dd1debdb2d
Kevin Koster <koster.k2001@gmail.com>
2020-05-18 11:43:46 +0200

    + added generate_password() to Model + added getPassword() to Model

--- src/eindopdracht/Model.java
+++ src/eindopdracht/Model.java
@@ -1,4 +1,29 @@
 package eindopdracht;
 
-public class Model {
+import java.util.Observable;
+import java.util.Random;
+
+public class Model extends Observable {
+    String password;
+
+    public void generate_password(int password_length) {
+        int leftLimit = 97;
+        int rightLimit = 122;
+        Random random = new Random();
+        StringBuilder buffer = new StringBuilder(password_length);
+        for (int i = 0; i < password_length; i++) {
+            int randomLimitedInt = leftLimit + (int)
+                    (random.nextFloat() * (rightLimit - leftLimit + 1));
+            buffer.append((char) randomLimitedInt);
+        }
+        password = buffer.toString();
+
+        setChanged();
+        notifyObservers();
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
 }
